624c461c10f160b3241fd4e743e801ccef8a6451
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 21:10:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,14 @@ def happy_birthday(birthday_kids)
   end
 end
  def age_appropriate_birthday
-
+birthday_kids = {
+      "Timmy" => 9,
+      "Sarah" => 6,
+      "Amanda" => 27
+    }
+    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+  end
+end
   end
 
 
