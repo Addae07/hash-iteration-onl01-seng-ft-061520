7906af4642340669c83d122ef4d8639f54812de9
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 21:12:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,8 +8,11 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-end
- def age_appropriate_birthday
+
+
+
+
+
 birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
