00a0ea408c3f8afe2030cbb748c52ccd2af8d020
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 21:16:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,18 +7,12 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-
-def age_appropriate_birthday(birthday_kids)
+age_appropriate_birthday(birthday_kids)
      it "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids.each do |kids_name, age|
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
       "Amanda" => 27
 }
-
-end
 end
 end
